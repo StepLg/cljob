d545e352b330598af05da513f2465712be179516
Stephan Kountso aka StepLg <steplg@gmail.com>
2011-01-11 10:06:31 +0300

    package info

---
+++ cljob/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Provide an easy way to run shell commands, upload and download files
+from large number of target macheans.
+"""
+
+__author__  = '''Stephan Kountso aka StepLg'''
+__email__   = '''StepLg@gmail.com'''
+
+__url__     = '''http://github.com/steplg/cljob'''
+__license__ = '''PSF (Python Software Foundation)'''
+
+__version__ = 0.1
+
--- setup.py
+++ setup.py
@@ -2,10 +2,49 @@
 
 from distutils.core import setup
 from glob import glob
+from cljob import __author__,  \
+                  __email__,   \
+                  __license__, \
+                  __url__,     \
+                  __version__
 
 setup(
     name='cljob',
-    version='0.1-dev',
+    version=str(__version__),
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
+    url=__url__,
     packages=['cljob'],
-    scripts=glob('./trsh*') + ['./tcheck_fails']
+    scripts=glob('./trsh*') + ['./tcheck_failures'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Topic :: System :: Clustering",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities"
+    ],
+
+    description="Cluster Administrators' ssh Wrapper (run shell cmds, upload, download)",
+    long_description='''
+
+    cljob is yet another wrapper around ssh/rsh for running jobs on multiple
+    targets. At the moment you can run shell commands, upload and download
+    files from target machenes. It can support thousands of targets and runs
+    a configurable number of jobs concurrently. It separately gathers results,
+    output and error messages, displaying summary/status information it comes
+    in (asynchronously) and more detailed data after all jobs have completed.
+
+    cljob has a very simple and clear internal structure, so you can easyly add
+    your own functionality (for example, up/download files not via rsync).
+
+    Original idea from classh (http://pypi.python.org/pypi/classh).
+    '''
 )
